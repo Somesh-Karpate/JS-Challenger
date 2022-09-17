6cabbfd59f7a5b12a46539da18c5db1c589e10ef
Somesh-Karpate <karpatesomesh09@gmail.com>
2022-09-17 11:25:46 +0530

    nonprimitive

---
+++ JSFundamentals/JSbasics/nonprimitivedatatypes.js
@@ -0,0 +1,25 @@
+//Non-primitive datatypes in javascript
+// Array
+
+var names = ["somesh","karpate"]; //Arrays of string
+var ages = [21,20]; //Arrays of number
+console.log(names);
+console.log(ages);
+
+
+// Object
+var student={
+    rollno : 41,
+    name : "SOmesh",
+    age : 20,
+    city : "Mumbai"
+};
+console.log(student);
+
+// Function
+function add(a,b){
+    let sum = a+b;
+    console.log(sum);
+}
+add(2,3);
+add(45,72);
