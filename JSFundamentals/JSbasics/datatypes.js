b5ae0d3ecbb1519b1ba1e308c8bbe11199c7037d
Somesh-Karpate <karpatesomesh09@gmail.com>
2022-09-17 11:14:21 +0530

    datatypes

---
+++ JSFundamentals/JSbasics/datatypes.js
@@ -0,0 +1,23 @@
+// Primitive Datatypes in javascript
+// number
+// string
+// Boolean
+// undefined
+// null
+// BigInt
+// Symbol
+
+let number = 1;
+let string = "Somesh";
+let bool = true;
+let u;
+let n= null;
+const alsoHuge = BigInt(9007199254740991);
+const sym2 = Symbol('foo');
+console.log(typeof(number));
+console.log(typeof(string));
+console.log(typeof(bool));
+console.log(typeof(u));
+console.log(typeof(n));
+console.log(typeof(alsoHuge));
+console.log(typeof(sym2));
